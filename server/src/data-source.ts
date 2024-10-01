2588ec9e8b018ba05d4183e71e74f1d693571390
규 서문 <illsi@naver.com>
2024-10-02 00:35:31 +0900

    커밋

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,17 +1,18 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
+    username: "postgres",
+    password: "password",
+    database: "postgres",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [
+        "src/entities/**/*.ts"
+    ],
     migrations: [],
     subscribers: [],
 })
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express"
 import morgan from "morgan"
 import { AppDataSource } from "./data-source"
 
-const app =express();
+const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
