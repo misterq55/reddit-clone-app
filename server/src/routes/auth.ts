1cdb2ee59098cd38cc226f6f230b88667bb08370
규 서문 <illsi@naver.com>
2024-10-23 22:15:08 +0900

    회원 가입 페이지 기능 생성(3) 진행중

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,36 @@
 import { Request, Response, Router } from "express";
+import { User } from "../entities/User"
+import { validate } from "class-validator";
 
 const register = async (req: Request, res: Response) => {
     const {email, username, password} = req.body;
-    console.log('eamil', email);
+    
+    try {
+        let errors: any = {};
+
+        const emailUser = await User.findOneBy({email});
+        const usernameUser  = await User.findOneBy({username});
+
+        if (emailUser) errors.email = "이미 해당 이메일 주소가 사용되었습니다."
+        if (usernameUser) errors.username = "이미 이 사용자 이름이 사용되었습니다."
+
+        if (Object.keys(errors).length > 0) {
+            return res.status(400).json(errors)
+        }
+
+        const user = new User();
+        user.email = email;
+        user.username = username;
+        user.password = password;
+
+        errors = await validate(user);
+
+        await user.save();
+        return res.json(user);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error });
+    }
 }
 
 const router = Router();
