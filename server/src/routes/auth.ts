f346d6c99b7f9ed1cf3e9b896d12bf583f2be256
규 서문 <illsi@naver.com>
2025-03-13 17:56:15 +0900

    가입/로그인

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,6 +2,14 @@ import { Request, Response, Router } from "express";
 import { User } from "../entities/User"
 import { validate } from "class-validator";
 
+const mapError = (errors: Object[]) => {
+    return errors.reduce((prev: any, err: any) => {
+        prev[err.property] = Object.entries(err.constraints[0][1])
+        
+        return prev;
+    }, {})
+}
+
 const register = async (req: Request, res: Response) => {
     const {email, username, password} = req.body;
     
@@ -14,9 +22,9 @@ const register = async (req: Request, res: Response) => {
         if (emailUser) errors.email = "이미 해당 이메일 주소가 사용되었습니다."
         if (usernameUser) errors.username = "이미 이 사용자 이름이 사용되었습니다."
 
-        if (Object.keys(errors).length > 0) {
-            return res.status(400).json(errors)
-        }
+        // if (Object.keys(errors).length > 0) {
+        //     return res.status(400).json(errors)
+        // }
 
         const user = new User();
         user.email = email;
@@ -24,12 +32,13 @@ const register = async (req: Request, res: Response) => {
         user.password = password;
 
         errors = await validate(user);
+        console.log('errors', errors)
 
         await user.save();
-        return res.json(user);
+        // return res.json(user);
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ error });
+        // return res.status(500).json({ error });
     }
 }
 
